43f37d16a70ebc8a610f9c11a0ae1d3dd9ee8732
bettyzry <49117825+bettyzry@users.noreply.github.com>
2022-11-09 18:03:56 +0800

    delete useless files

--- FMMS/run_FMMS.py
+++ FMMS/run_FMMS.py
@@ -1,168 +1,122 @@
 import pandas as pd
 
 import config
-import evaluation
 import utils
 from FMMS import FMMS
 import torch
 import torch.utils.data as Data
-import pickle
+import numpy as np
+from gene_feature import generate_meta_features
+
+optlsit = {
+    'sgd': torch.optim.SGD,
+    'adam': torch.optim.Adam,
+    'adagrad': torch.optim.Adagrad}
+
+losslist = {
+    'rmse': utils.rmse_loss, 'mse': utils.mse_loss,
+    'cos': utils.cos_loss, 'L1': utils.l1_loss,
+    'sL1': utils.SmoothL1_loss, 'kd': utils.KLDiv_loss,
+    'DCG': utils.DCG_loss}
 
 
-def Train(params, train_params, xtrain, xvalid, xtest, ytrain, yvalid, ytest, txt=''):
-    path = config.get_para(train_params, params)
-    train_dataset = Data.TensorDataset(torch.tensor(xtrain), torch.tensor(ytrain))
-    # 训练集分批处理
-    loader = Data.DataLoader(
-        dataset=train_dataset,  # torch TensorDataset format
-        batch_size=train_params['batch'],  # 最新批数据
-        shuffle=False  # 是否随机打乱数据
-    )
-    fmms = FMMS(**params)
-    # 训练网络
-    optimizer = train_params['opt'](fmms.parameters(), lr=train_params['lr'])
-    loss_train_set = []
-    loss_valid_set = []
-    loss_test_set = []
-    ERank_train = []
-    ETopn_train = []
-    ERank_valid = []
-    ETopn_valid = []
-    ERank_test = []
-    ETopn_test = []
-    np_ctr = 1
-    for epoch in range(train_params['epoch']):  # 对数据集进行训练
-        # improvement is smaller than 1 perc
-        if config.dataset == 'pmf':
-            if epoch >= 2 and (loss_valid_set[-2] - loss_valid_set[-1])/loss_valid_set[-1] <= 0.001:
-                np_ctr += 1
-            else:
-                np_ctr = 1
-            if np_ctr > 5:
-                break
+class run_FMMS():
+    def __init__(self, Fmap, Pmap, Fmapvalid=None, Pmapvalid=None, embedding_size=4, batch=4, lr=0.001, epoch=50, opt='adam', loss='cos'):
+        """
+        Parameters
+        ----------
+        Fmap: The feature Map of the historical dataset (D*F)
+        Pmap: The performance of the candidate models on the historical dataset (M*D)
+        embedding_size: The dimension of the auxiliary vector
+        batch: Batch size
+        lr: Learning rate
+        epoch: Training epoch
+        opt: Optimizer
+        loss: Loss function
+        """
+        self.feature_size = Fmap.shape[1]
+        self.model_size = Pmap.shape[1]
+        self.fmms = FMMS(feature_size=self.feature_size, model_size=self.model_size, embedding_size=embedding_size)
+        self.Fmap = Fmap
+        self.Pmap = Pmap
+        self.Fmapvalid = Fmapvalid
+        self.Pmapvalid = Pmapvalid
+        self.batch = batch
+        self.lr = lr
+        self.epoch = epoch
+        self.opt = optlsit[opt]
+        self.loss = losslist[loss]
+        return
 
-        for step, (batch_x, batch_y) in enumerate(loader):  # 每个训练步骤
-            # 此处省略一些训练步骤
-            optimizer.zero_grad()  # 如果不置零，Variable的梯度在每次backwrd的时候都会累加
-            output = fmms(batch_x)
-            # 平方差
-            loss_train = train_params['loss'](output, batch_y)
-            l2_regularization = torch.tensor(0).float()
-            # 加入l2正则
-            for param in fmms.parameters():
-                l2_regularization += torch.norm(param, 2)
-            # loss = rmse_loss + l2_regularization
-            loss_train.backward()
-            # loss_train.backward(torch.ones(train_params['batch'], model_size))
-            optimizer.step()  # 进行更新
-            print("batch loss:", step, loss_train.item())
-        # 将每一次训练的数据进行存储，然后用于绘制曲线
+    def fit(self, save=False):
+        train_dataset = Data.TensorDataset(torch.tensor(self.Fmap), torch.tensor(self.Pmap))
+        loader = Data.DataLoader(
+            dataset=train_dataset,  # torch TensorDataset format
+            batch_size=self.batch,  # 最新批数据
+            shuffle=False  # 是否随机打乱数据
+        )
+        optimizer = self.opt(self.fmms.parameters(), lr=self.lr)
+        loss_valid_set = []
+        np_ctr = 1      # 提升幅度小于0.1%的累计次数
+        for epoch in range(self.epoch):  # 对数据集进行训练
+            # 早停机制
+            if self.Fmapvalid is not None:
+                if epoch >= 2 and (loss_valid_set[-2] - loss_valid_set[-1]) / loss_valid_set[-1] <= 0.001:
+                    np_ctr += 1
+                else:
+                    np_ctr = 1
+                if np_ctr > 5:
+                    break
 
-        # train
-        ytrainpred = fmms(torch.tensor(xtrain))
-        loss_train = train_params['loss'](ytrainpred, ytrain)
-        ytrainpred = ytrainpred.detach().numpy()
-        train_rank, _ = evaluation.ERank(ytrainpred, ytrain)
-        train_topn, _ = evaluation.ETopnk(ytrainpred, ytrain, int(config.modelnum*0.05), 5)
-        loss_train_set.append(loss_train.item())
-        ETopn_train.append(train_rank)
-        ERank_train.append(train_topn)
+            # train
+            for step, (batch_x, batch_y) in enumerate(loader):  # 每个训练步骤
+                # 此处省略一些训练步骤
+                optimizer.zero_grad()  # 如果不置零，Variable的梯度在每次backwrd的时候都会累加
+                output = self.fmms(batch_x)
+                # 平方差
+                loss_train = self.loss(output, batch_y)
+                l2_regularization = torch.tensor(0).float()
+                # 加入l2正则
+                for param in self.fmms.parameters():
+                    l2_regularization += torch.norm(param, 2)
+                # loss = rmse_loss + l2_regularization
+                loss_train.backward()
+                # loss_train.backward(torch.ones(train_params['batch'], model_size))
+                optimizer.step()  # 进行更新
+                print("batch loss:", step, loss_train.item())
 
-        # valid
-        yvalidpred = fmms(torch.tensor(xvalid))
-        loss_valid = train_params['loss'](yvalidpred, yvalid)
-        yvalidpred = yvalidpred.detach().numpy()
-        valid_rank, _ = evaluation.ERank(yvalidpred, yvalid)
-        valid_topn, _ = evaluation.ETopnk(yvalidpred, yvalid, int(config.modelnum*0.05), 5)
-        loss_valid_set.append(loss_valid.item())
-        ETopn_valid.append(valid_rank)
-        ERank_valid.append(valid_topn)
+            # valid
+            if self.Fmapvalid is not None:
+                Pmapvalidpred = self.fmms(torch.tensor(self.Fmapvalid))
+                loss_valid = self.loss(Pmapvalidpred, self.Pmapvalid)
+                loss_valid_set.append(loss_valid.item())
 
-        # test
-        ytestpred = fmms(torch.tensor(xtest))
-        loss_test = train_params['loss'](ytestpred, ytest)
-        ytestpred = ytestpred.detach().numpy()
-        test_rank, _ = evaluation.ERank(ytestpred, ytest)
-        test_topn, _ = evaluation.ETopnk(ytestpred, ytest, int(config.modelnum*0.05), 5)
-        loss_test_set.append(loss_test.item())
-        ETopn_test.append(test_topn)
-        ERank_test.append(test_rank)
+            print("epoch: %d" % epoch, "loss_train:", loss_train.item())
+        # # 保存训练好的模型
+        if save:
+            torch.save(self.fmms.state_dict(), "models/fmms.pt")
 
-        print("epoch: %d" % epoch, "loss_train:", loss_train.item(), "loss_test", loss_test.item())
-    # print(y_train[0:5],"  ",output[0:5])
-    # plot.plot_convergence(loss_train_set, loss_test_set, path='fmms_'+path)
-    # # 保存训练好的模型
-    torch.save(fmms.state_dict(), "models/%s/FMMS_%s_%s.pt" % (config.dataset, path, txt))
-    log = {
-        'loss_train_set': loss_train_set,
-        'loss_valid_set': loss_valid_set,
-        'loss_test_set': loss_test_set,
-        'ETopn_train': ETopn_train,
-        'ERank_train': ERank_train,
-        'ETopn_valid': ETopn_valid,
-        'ERank_valid': ERank_valid,
-        'ETopn_test': ETopn_test,
-        'ERank_test': ERank_test
-    }
-    log_df = pd.DataFrame(log)
-    return log_df
-
-
-def Test(params, train_params, x_test, y_test, txt=''):
-    # test
-    path = config.get_para(train_params, params)
-    print(path)
-    fmms = FMMS(**params)
-    fmms.load_state_dict(torch.load("models/%s/FMMS%s_%s.pt" % (config.dataset, path, txt)))
-    pred = fmms(torch.tensor(x_test))
-    loss = train_params['loss'](pred, y_test)
-    pred = pred.detach().numpy()
-    print("test_loss", loss)
-    result = {'fmms': pred}
-    pickle.dump(result, open("results/%s/result_fmms_%s_%s.pkl" % (config.dataset, path, txt), 'wb'))
-
-    return pred
-
-
-def main(txt=''):
-    rate = config.get_rate()
-    ytrain, ytest, xtrain, xtest = utils.get_data2(rate)
-    ytrain = ytrain[:, :config.modelnum]
-    ytest = ytest[:, :config.modelnum]
-
-    optlsit = {'sgd': torch.optim.SGD, 'adam': torch.optim.Adam, 'adagrad': torch.optim.Adagrad}
-    losslist = {'rmse': utils.rmse_loss, 'mse': utils.mse_loss,
-                'cos': utils.cos_loss, 'L1': utils.l1_loss,
-                'sL1': utils.SmoothL1_loss, 'kd': utils.KLDiv_loss,
-                'DCG': utils.DCG_loss
-                }
-    params = {
-        'embedding_size': 4,
-        'feature_size': xtrain.shape[1],
-        'model_size': ytrain.shape[1],
-        'layer_size': 3,
-        'hiddensize': 64
-    }
-    opt = 'adam'
-    l = 'cos'
-    train_params = {
-        'batch': 4,
-        'lr': 0.001,
-        'epoch': 50,
-        'opt': optlsit[opt],
-        'optname': opt,
-        'loss': losslist[l],
-        'lossname': l,
-    }
-    # ytrain = utils.scaleY(ytrain)
-    # ytest = utils.scaleY(ytest)
-    xtrain, ytrain, xvalid, yvalid = utils.train_test_val_split(xtrain, ytrain, rate)
-    log = Train(params, train_params, xtrain, xvalid, xtest, ytrain, yvalid, ytest)
-    # log.to_csv('./experiments/convergence/fmms%s_%s.csv'%(config.get_para(train_params, params),txt))  # fmms在pmf数据集上参数为path时第r次实验的中间结果
-    ypred = Test(params, train_params, xtest, ytest)
-    evaluation.ERank(ypred, ytest, 'fmms')                                          # 所推荐的模型的平均排名
-    evaluation.ETopnk(ypred, ytest, int(config.modelnum*0.05), 5, 'fmms')           # 所推荐的模型中，排在前n个的里有几个
+    def predict(self, x=None, f=None, topn=5, load=False):
+        if load:
+            self.fmms.load_state_dict(torch.load("models/fmms.pt"))
+        if f is None:
+            f = generate_meta_features(x)[0]
+        if f.shape[0] != self.feature_size:
+            print('The feature size of historical dataset (%d) and target dataset (%d) is not match' % (f.shape[0], self.feature_size))
+        pred = self.fmms(torch.tensor([f]))         # 预测的性能得分（仅保证相对排序准确，不保证绝对值准确）
+        pred = pred.detach().numpy()[0]
+        argsort_pred = np.argsort(pred)             # 对应的排名
+        recommend = argsort_pred[::-1]
+        print("预测性能最优的%d位模型的索引值:" % topn, recommend[:topn])
+        return
 
 
 if __name__ == '__main__':
-    main()
+    ptrain, ptest, ftrain, ftest = utils.get_data(0.1)
+    ptrain = ptrain[:, :config.modelnum]
+    ptest = ptest[:, :config.modelnum]
+    ftrain, ptrain, fvalid, pvalid = utils.train_test_val_split(ftrain, ptrain, 0.1)
+    rfmms = run_FMMS(ftrain, ptrain, fvalid, pvalid)
+    rfmms.fit(save=True)
+    rfmms.predict(f=ftest[0], load=True)
+
