9069b3dd7ea4c2310def2b0946284cbbbbc2724f
xuhongzuo <hongzuoxu@126.com>
2023-09-06 18:25:54 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'requirements
 
 setuptools.setup(
     name="deepod",
-    version="0.4.0",
+    version="0.4.1",
     author="Hongzuo Xu",
     author_email="hongzuoxu@126.com",
     description="",
