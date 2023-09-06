009961ae316755226190d2c712af79bdfea9e965
xuhongzuo <hongzuoxu@126.com>
2023-09-06 16:27:48 +0800

    fix bugs in metrics

--- deepod/metrics/__init__.py
+++ deepod/metrics/__init__.py
@@ -1,8 +1,9 @@
-from _anomaly_detection import auc_roc
-from _anomaly_detection import auc_pr
-from _anomaly_detection import tabular_metrics
-from _anomaly_detection import ts_metrics
-from _tsad_adjustment import point_adjustment
+from deepod.metrics._anomaly_detection import auc_roc
+from deepod.metrics._anomaly_detection import auc_pr
+from deepod.metrics._anomaly_detection import tabular_metrics
+from deepod.metrics._anomaly_detection import ts_metrics
+from deepod.metrics._tsad_adjustment import point_adjustment
+
 
 __all__ = [
     'auc_pr',
