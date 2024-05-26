7654813835cd79dc621def2434519e2de86cf858
xuhongzuo <hongzuoxu@126.com>
2024-05-26 10:14:55 +0800

    remove unused code in metrics

--- deepod/metrics/vus/metrics.py
+++ deepod/metrics/vus/metrics.py
@@ -4,7 +4,7 @@ from deepod.metrics.vus.utils.metrics import metricor
 def get_range_vus_roc(score, labels, slidingWindow):
     R_AUC_ROC, R_AUC_PR, _, _, _ = metricor().RangeAUC(labels=labels, score=score,
                                                        window=slidingWindow, plot_ROC=True)
-    VUS_ROC, VUS_PR = metricor().RangeAUC_volume(labels_original=labels,
+    _, _, _, _, VUS_ROC, VUS_PR = metricor().RangeAUC_volume(labels_original=labels,
                                                  score=score,
                                                  windowSize=2*slidingWindow)
 
